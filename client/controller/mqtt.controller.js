09b4cc096e6666efb38c54fba4e2acc6ebc8fdc8
Paco Navarro <packarbell@gmail.com>
2017-11-07 20:03:18 +0100

    tests with mqtt

---
+++ client/controller/mqtt.controller.js
@@ -0,0 +1,16 @@
+var mqtt = require('mqtt');
+// var client  = mqtt.connect('mqtt://iot-hub-pako.azure-devices.net');
+var client  = mqtt.connect('mqtt://test.mosquitto.org')
+
+client.on('connect', function () {
+  client.subscribe('presence')
+  client.publish('presence', 'Hello mqtt')
+})
+ 
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  client.end()
+})
+
+
--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 module.exports = function (app) {  
     const router = require('./router');  
     const io = require('./controller/io.controller');
+    const mqtt = require('./controller/mqtt.controller');
   
     app.use('/api', router);
     io.init();
