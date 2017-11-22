3832bb5305fb6a3900d2a0400ed7336de36a8b07
Paco Navarro <packarbell@gmail.com>
2017-11-22 22:10:14 +0100

    run

--- test/temp.spec.js
+++ test/temp.spec.js
@@ -1,12 +1,14 @@
 var PythonShell = require('python-shell');
 var script = '/root/Adafruit_Python_DHT/examples/temperature.py';
+var pyshell = new PythonShell(script);
 
-PythonShell.run(script, function (err) {
-  if (err) throw err;
-  console.log('finished');
-});
-
-PythonShell.on('message', function (message) {
+pyshell.on('message', function (message) {
   // received a message sent from the Python script (a simple "print" statement)
   console.log(message);
+});
+
+// end the input stream and allow the process to exit
+pyshell.end(function (err) {
+  if (err) throw err;
+  console.log('finished');
 });
