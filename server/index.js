dc18884e87103022bb6e1f18e1ff01196fc17bd1
Paco Navarro <packarbell@gmail.com>
2017-12-20 23:11:53 +0100

    graphic

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
-module.exports = function (app) {
+module.exports = function init(app) {
   const router = require('./router');
-  const devices = require('./controller/devices.controller');
+  const express = require('express');
+  // const devices = require('./controller/devices.controller');
+  // devices.init();
+
+  app.use('/lib', express.static('node_modules'));
+  app.use(express.static('server'));
 
   console.log('starting climateberry server');
   app.use('/api', router);
-  devices.init();
 };
--- server/router.js
+++ server/router.js
@@ -1,19 +1,9 @@
 function init() {
-  
   const router = require('express').Router();
-  const temperatureController = require('./controller/temperature.controller');
-  const loginController = require('./controller/login.controller');
-
-  router.get('/temperature', (req, res) => {
-    res.json(temperatureController.get(req));
-  });
-
-  router.put('/temperature', (req, res) => {
-    res.json(temperatureController.put(req));
-  });
+  
 
 
   return router;
 }
-  
+
 module.exports = init();
