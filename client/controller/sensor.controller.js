1b5ad87d2655d0744697cf173d781c15f63ba60b
Paco Navarro <packarbell@gmail.com>
2017-12-20 20:57:50 +0100

    reopen sensor

--- client/controller/sensor.controller.js
+++ client/controller/sensor.controller.js
@@ -1,26 +1,25 @@
 function temp() {
   return new Promise((resolve, reject) => {
-    resolve({});
-    // try {
-    //   const PythonShell = require('python-shell');
-    //   const pyshell = new PythonShell('readtemperature.py', {
-    //     // scriptPath: '/root/Adafruit_Python_DHT/examples/'
-    //   });
+    try {
+      const PythonShell = require('python-shell');
+      const pyshell = new PythonShell('readtemperature.py', {
+        // scriptPath: '/root/Adafruit_Python_DHT/examples/'
+      });
       
-    //   pyshell.on('message', function (message) {
-    //     console.log(message);
-    //     resolve(JSON.parse(message));
-    //   });
+      pyshell.on('message', function (message) {
+        console.log(message);
+        resolve(JSON.parse(message));
+      });
       
-    //   // end the input stream and allow the process to exit
-    //   pyshell.end(function (err) {
-    //     if (err) throw err;
-    //   });
+      // end the input stream and allow the process to exit
+      pyshell.end(function (err) {
+        if (err) throw err;
+      });
       
-    // } catch(err) {
-    //   console.warn(err);
-    //   reject(err);
-    // }
+    } catch(err) {
+      console.warn(err);
+      reject(err);
+    }
   });
 }
 
